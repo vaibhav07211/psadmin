38870650e6b40cc1e8895cdc45e20eda704e7838
vaibhav agrawal <vaibhav.agrawal@xcdify.com>
2018-01-27 11:59:38 +0530

    created the basic structure of app using react and custom routing

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div className="jumbotron">
+				<h1>PluralSight Administration</h1>
+				<p>React, React Router and Flux</p>
+			</div>
+		);
+	}
+});
+
+module.exports = Home;
